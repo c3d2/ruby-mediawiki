08f532a2342eaab841562dce957eee6a9d98605b
astro <astro@ba9c31aa-a806-0410-9a81-9f13d15ee83b>
2005-12-13 21:07:04 +0000

    New rdoc-invocation script Documentation of the basic Wiki methods
    git-svn-id: svn://78.47.249.61/ruby-mediawiki/trunk@34 ba9c31aa-a806-0410-9a81-9f13d15ee83b

--- lib/mediawiki.rb
+++ lib/mediawiki.rb
@@ -1,4 +1,12 @@
-#!/usr/bin/env ruby
+##
+# =Ruby-MediaWiki - manipulate MediaWiki pages from Ruby.
+#
+# Please note that documents spit out by MediaWiki *must* be valid
+# XHTML (or XML)!
+#
+# You may not want to use MediaWiki::Wiki directly but let MediaWiki.dotfile
+# create your instance. This gives you the power of the dotfile
+# infrastructure. See sample apps and <tt>mediawikirc.sample</tt>.
 
 require 'uri'
 
@@ -6,23 +14,35 @@ require 'mediawiki/article'
 require 'mediawiki/category'
 require 'mediawiki/minibrowser'
 
+
 module MediaWiki
   class Wiki
-    attr_accessor :browser
-  
-    def initialize(url, user = nil, password = nil)
-      #if url.class == Symbol
-      #  config = YAML.load_file("#{ENV['HOME']}/.mediawikirc")[url.to_s]
-      #  url = config['url']
-      #  user = config['user'] unless user
-      #  password = config['password'] unless password
-      #end
+    ##
+    # The MiniBrowser instance used by this Wiki.
+    # This must be readable as it's used by Article and Category
+    # to fetch themselves.
+    attr_reader :browser
 
+    ##
+    # Initialize a new Wiki instance.
+    # url:: [String] URL-Path to index.php (without index.php), may containt <tt>user:password</tt> combination.
+    # user:: [String] If not nil, log in with that MediaWiki username (see Wiki#login)
+    # password:: [String] If not nil, log in with that MediaWiki password (see Wiki#login)
+    def initialize(url, user = nil, password = nil)
       @url = URI.parse( url.match(/\/$/) ? url : url + '/' )
       @browser = MiniBrowser.new(@url)
       login( user, password ) if user and password
     end
 
+    ##
+    # Log in into MediaWiki
+    #
+    # This is *not* HTTP authentication
+    # (put HTTP-Auth into [url] of Wiki#initialize!)
+    # user:: [String] MediaWiki username
+    # password:: [String] MediaWiki password
+    #
+    # May raise an exception if cannot authenticate
     def login( username, password )
       data = {'wpName' => username, 'wpPassword' => password, 'wpLoginattempt' => 'Log in'}
       data = @browser.post_content( @url.path + 'index.php?title=Special:Userlogin&action=submitlogin', data )
@@ -31,14 +51,26 @@ module MediaWiki
       end
     end
 
+    ##
+    # Return a new Category instance with given name,
+    # will be constructed with [self] (for MiniBrowser usage)
+    # name:: [String] Category name (to be prepended with "Category:")
     def category(name)
       Category.new(self, name)
     end
 
+    ##
+    # Return a new Article instance with given name,
+    # will be constructed with [self] (for MiniBrowser usage)
+    # name:: [String] Article name
+    # section:: [Fixnum] Optional section number
     def article(name, section = nil)
       Article.new(self, name, section)
     end
 
+    ##
+    # Returns the pages listed on "Special:Allpages"
+    # result:: [Array] of [String] Articlenames
     def allpages()
       pages = []
       Article.new( self, 'Special:Allpages', nil, false ).xhtml.each_element('table[2]/tr/td/a') do | a |
@@ -47,6 +79,16 @@ module MediaWiki
       pages
     end
 
+    ##
+    # Construct the URL to a specific article
+    #
+    # Uses the [url] the Wiki instance was constructed with,
+    # appends "index.php", the name parameter and, optionally,
+    # the section.
+    #
+    # Often called by Article, Category, ...
+    # name:: [String] Article name
+    # section:: [Fixnum] Optional section number
     def article_url(name, section = nil)
       "#{@url.path}index.php?title=#{CGI::escape(name.gsub(' ', '_'))}#{section ? "&section=#{CGI::escape(section.to_s)}" : ''}"
     end
---
+++ mkrdoc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rdoc --inline-source --line-numbers --op rdoc lib/mediawiki.rb lib/mediawiki/*.rb
