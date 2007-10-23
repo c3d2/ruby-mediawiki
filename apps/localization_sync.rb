9330c0a9e96869602b43d9073734974b1284a43c
astro <astro@ba9c31aa-a806-0410-9a81-9f13d15ee83b>
2007-10-23 16:54:52 +0000

    README COPYING GPLv3 headers in all .rb files
    git-svn-id: svn://78.47.249.61/ruby-mediawiki/trunk@72 ba9c31aa-a806-0410-9a81-9f13d15ee83b

--- apps/localization_sync.rb
+++ apps/localization_sync.rb
@@ -1,4 +1,22 @@
 #!/usr/bin/env ruby
+=begin
+    This file is part of Ruby-MediaWiki.
+
+    Ruby-MediaWiki is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Ruby-MediaWiki is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ruby-MediaWiki.  If not, see
+    <http://www.gnu.org/licenses/>.
+=end
+
 
 # this bot expects a fully functional local pentabarf installation
 
